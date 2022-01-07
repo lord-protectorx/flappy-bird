cf8ba7e977bd4465cdc265ce2428d4f8ad5ad362
Togrul <togrul.ibragimov.2015@mail.ru>
2022-01-07 21:41:28 +0300

    Изменена высота прыжка и скорость в режиме 'HARD'

---
+++ score_saver.py
@@ -0,0 +1,19 @@
+import os
+# прописал чтобы не ругалось, вместо int(), тут должно быть значение реальное
+score = int()
+
+files = os.listdir("./data")
+if 'best_score.txt' in files:
+    file = open('./data/best_score.txt', 'r', encoding="utf-8")
+    best_score = file.read()
+    if score > int(best_score):
+        best_score = str(score)
+    file.close()
+    file = open('./data/best_score.txt', 'w', encoding="utf-8")
+    file.write(str(best_score))
+    file.close()
+else:
+    file = open('./data/best_score.txt', 'w', encoding="utf-8")
+    file.write(str(score))
+    file.close()
+    print(file, 'FD')
