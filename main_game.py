e24897555763175ba72ec12d4cf16e7ab6100a51
Мария <user@gmail.com>
2022-01-07 23:37:26 +0300

    game over 1 cycle

--- main_game.py
+++ main_game.py
@@ -7,9 +7,15 @@ FPS = 50
 
 pygame.init()
 
+
 size = width, height = 600, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+f1 = pygame.font.Font('data/Flappy-Bird.ttf', 50)
+btn_size = 205
+all_sprites = pygame.sprite.Group()
+bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -43,6 +49,33 @@ def terminate():
     sys.exit()
 
 
+def gameover_screen(score):
+    intro_text = [f'Score: {score}']
+
+    fon = pygame.transform.scale(load_image('background_over.png'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font('data/Flappy-Bird.ttf', 40)
+    text_coord = 250
+    for line in intro_text:
+        string_rendered = font.render(line, True, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 230
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                terminate()  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
 def start_screen():
     intro_text = ['Best score']
 
@@ -65,15 +98,9 @@ def start_screen():
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                choose_win()  # выбираем сложность
         pygame.display.flip()
         clock.tick(FPS)
-
-
-start_screen()
-
-btn_size = 205
-all_sprites = pygame.sprite.Group()
 
 
 class Easy(pygame.sprite.Sprite):
@@ -113,8 +140,8 @@ class Hard(pygame.sprite.Sprite):
 def choose_win():
     background1 = pygame.transform.chop(load_image("background_win.png"), (0, 300, 300, 0))
     background2 = pygame.transform.scale(background1, (600, 600))
-    Hard(all_sprites)
-    Easy(all_sprites)
+    hard = Hard(all_sprites)
+    easy = Easy(all_sprites)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -126,9 +153,6 @@ def choose_win():
         all_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-
-
-choose_win()
 
 
 def draw_platform():
@@ -176,8 +200,7 @@ class Bird(pygame.sprite.Sprite):
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[self.cur_frame]
         self.counter += 1
-        if not pygame.sprite.collide_mask(self, btm_pipe) and not pygame.sprite.collide_mask(self,
-                                                                                               top_pipe) and self.flag:
+        if self.flag:
             self.speed += Bird.gravity
             self.rect = self.rect.move(0, self.speed)
         if self.rect.y > 530 or self.rect.y < 5:
@@ -198,7 +221,7 @@ class Bird(pygame.sprite.Sprite):
 
 
 class Pipe(pygame.sprite.Sprite):
-    def __init__(self, posis, *group):
+    def __init__(self, posis, x, y, *group):
         super().__init__(*group)
         self.image = pygame.transform.scale2x(load_image("pipe_up.png"))
         self.rect = self.image.get_rect()
@@ -207,9 +230,9 @@ class Pipe(pygame.sprite.Sprite):
 
         if posis == 1:  # top
             self.image = pygame.transform.flip(self.image, False, True)
-            self.rect.bottomleft = [x_pos, y_pos - int(pipe_per / 2)]
+            self.rect.bottomleft = [x, y - int(pipe_per / 2)]
         if posis == -1:  # bottom
-            self.rect.topleft = [x_pos, y_pos + int(pipe_per / 2)]
+            self.rect.topleft = [x, y + int(pipe_per / 2)]
 
     def update(self):
         self.rect.x -= speed
@@ -217,48 +240,49 @@ class Pipe(pygame.sprite.Sprite):
             self.kill()
 
 
-f1 = pygame.font.Font('data/Flappy-Bird.ttf', 50)
-bird_group = pygame.sprite.Group()
-bird = Bird()
-count = 0
-pipe_group = pygame.sprite.Group()
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-            Bird.jump(bird)
+def main_game():
+    global count, pos_x, last_pipe
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                Bird.jump(bird)
 
-    screen.blit(back_age2, (0, 0))
-    pos_x -= speed
+        screen.blit(back_age2, (0, 0))
+        pos_x -= speed
 
-    pipe_group.update()
-    pipe_group.draw(screen)
-    Now_time = pygame.time.get_ticks()
-    if Now_time - last_pipe > pipe_range:
-        pipe_heig = random.randint(-90, 90)
-        y_pos = 300 + pipe_heig
-        x_pos = 600
-        btm_pipe = Pipe(-1, pipe_group)
-        top_pipe = Pipe(1, pipe_group)
-        last_pipe = Now_time
-        count += 1
+        pipe_group.update()
+        pipe_group.draw(screen)
+        Now_time = pygame.time.get_ticks()
+        if Now_time - last_pipe > pipe_range:
+            pipe_heig = random.randint(-90, 90)
+            y_pos = 300 + pipe_heig
+            x_pos = 600
+            btm_pipe = Pipe(-1, x_pos, y_pos, pipe_group)
+            top_pipe = Pipe(1, x_pos, y_pos, pipe_group)
+            last_pipe = Now_time
+            count += 1
 
-    draw_platform()
-    if pos_x <= -72:
-        pos_x = 0
+        draw_platform()
+        if pos_x <= -72:
+            pos_x = 0
 
-    score = count - 2
-    bird_group.draw(screen)
-    bird_group.update()
-    if not bird.flag:
-        running = False
-    if pygame.sprite.groupcollide(bird_group, pipe_group, False, False):
-        running = False
-    if score >= 0:
-        text1 = f1.render(f'{score}', True, (255, 255, 255))
-        screen.blit(text1, (300, 50))
-    pygame.display.flip()
-    clock.tick(60)
-pygame.quit()
+        score = count - 1
+        bird_group.draw(screen)
+        bird_group.update()
+        if not bird.flag:
+            gameover_screen(score)
+        if pygame.sprite.groupcollide(bird_group, pipe_group, False, False):
+            gameover_screen(score)
+        if score >= 0:
+            text1 = f1.render(f'{score}', True, (255, 255, 255))
+            screen.blit(text1, (300, 50))
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
+
+# истсинный игровой цикл
+start_screen()
+# main_game()
